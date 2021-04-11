e507a991975b43a5e2570965eb77c9db58ebfe5a
jeferson-gallo <jdgap9801@gmail.com>
2021-04-11 17:00:56 -0500

    warnings fixed

--- app/src/main/java/com/eljeff/p3resistancecalculator/ColorsArrayAdapter.kt
+++ app/src/main/java/com/eljeff/p3resistancecalculator/ColorsArrayAdapter.kt
@@ -5,24 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
-import kotlinx.android.synthetic.main.activity_main.view.*
 import kotlinx.android.synthetic.main.spinner_digits.view.*
 
 class ColorsArrayAdapter (context: Context, colorsList: List<ColorDigits>) : ArrayAdapter<ColorDigits>(context, 0, colorsList)  {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
-        return initView(position, convertView, parent)
+        return parent.initView(position)
     }
 
     override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup): View {
-        return initView(position, convertView, parent)
+        return parent.initView(position)
     }
 
-    private fun initView(position: Int, convertView: View?, parent: ViewGroup) : View {
+    private fun ViewGroup.initView(position: Int) : View {
 
         val color = getItem(position)
 
-        val view = LayoutInflater.from(context).inflate(R.layout.spinner_digits, parent, false)
+        val view = LayoutInflater.from(this@ColorsArrayAdapter.context).inflate(R.layout.spinner_digits,
+            this, false)
 
         view.imgvw_color.setImageResource(color!!.image)
         view.txvw_color_name.text = color.name
